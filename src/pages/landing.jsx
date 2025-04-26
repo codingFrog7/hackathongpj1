d808536694c2f834d1ed53b89c4f45aac0c25cbd
Deepu kashyap <109035551+codingFrog7@users.noreply.github.com>
2025-04-26 11:46:51 +0530

    updated

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -16,71 +16,79 @@ export default function AirbnbHome() {
   const listings = [
     {
       id: 1,
-      title: "Minimalist Studio in Bangalore",
-      location: "Indiranagar, Bangalore",
-      price: "₹1,800/night",
-      rating: 4.6,
-      image: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=800&h=600&fit=crop"
+      title: "Affordable Boys PG in Sector 62",
+      location: "Noida, Uttar Pradesh",
+      price: "₹6,000/month",
+      rating: 4.2,
+      image: "https://media.istockphoto.com/id/474474056/photo/interior-of-a-children-bedroom.webp?a=1&b=1&s=612x612&w=0&k=20&c=DsJbZndsLUbnlOU_wGm9570Ntj0D1kt_0pYyOJsiC3g=",
+      type: "Boys PG"
     },
     {
       id: 2,
-      title: "Cozy Apartment in Mumbai",
-      location: "Bandra West, Mumbai",
-      price: "₹2,500/night",
+      title: "Girls PG near Viman Nagar",
+      location: "Pune, Maharashtra",
+      price: "₹7,500/month",
       rating: 4.8,
-      image: "https://images.unsplash.com/photo-1720420021124-4e18564e070f?q=80&w=1392&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      image: "https://images.unsplash.com/photo-1720420021124-4e18564e070f?q=80&w=1392&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      type: "Girls PG",
     },
     {
       id: 3,
-      title: "Peaceful Stay in Goa",
-      location: "Anjuna, Goa",
-      price: "₹2,000/night",
-      rating: 4.7,
-      image: "https://plus.unsplash.com/premium_photo-1661846577575-560fd37a2a19?q=80&w=1437&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title: "Shared Rental in Koramangala",
+      location: "Bangalore, Karnataka",
+      price: "₹8,000/month",
+      rating: 4.0,
+      image: "https://media.istockphoto.com/id/665182294/photo/construction-materials-furniture-and-phone-are-on-the-floor-of-apartment-in-the-hotel-during.webp?a=1&b=1&s=612x612&w=0&k=20&c=Q0vpDXZTZ3Dubk8uCsJVVnztW8VAcrDG4_1Zj2F5ZP0=",
+      type: "Rental Room",
     },
     {
       id: 4,
-      title: "Hill View Room in Manali",
-      location: "Old Manali, Himachal Pradesh",
-      price: "₹1,200/night",
-      rating: 4.5,
-      image: "https://images.unsplash.com/photo-1612320582615-efd6f0c78f4b?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title: "Budget PG for Students in Kothrud",
+      location: "Pune, Maharashtra",
+      price: "₹5,500/month",
+      rating: 3.8,
+      image: "https://media.istockphoto.com/id/466227744/photo/interior-of-a-children-bedroom.jpg?s=612x612&w=0&k=20&c=gOtUcREyY_VL8BBA2rtuIFdFr1kR5xIKw81J7NxvVzs="
+      , type: "Student PG",
     },
     {
       id: 5,
-      title: "Luxury Flat in Delhi",
-      location: "Hauz Khas, New Delhi",
-      price: "₹3,000/night",
-      rating: 4.9,
-      image: "https://images.unsplash.com/photo-1669664863985-ac4a81e7483e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title: "Girls Hostel near North Campus",
+      location: "New Delhi, Delhi",
+      price: "₹7,000/month",
+      rating: 4.7,
+      image: "https://media.istockphoto.com/id/1375892095/photo/organized-bedroom-at-home-or-hotel-conept-of-hygiene-service-and-well-maintained-hospitality.jpg?s=612x612&w=0&k=20&c=rqfokjPpaI2Sa-XFHufeStxWfks6HxYK4OKjX7DyD_A="
+      , type: "Girls Hostel"
     },
     {
       id: 6,
-      title: "Lakefront Home in Udaipur",
-      location: "Fatehsagar, Udaipur",
-      price: "₹2,200/night",
-      rating: 4.6,
+    title: "Working Men’s PG in Ameerpet",
+    location: "Hyderabad, Telangana",
+    price: "₹6,200/month",
+    rating: 4.1,
       image: "https://images.unsplash.com/photo-1708616178113-f687b1113414?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ,    type: "Mens PG",
     },
     {
       id: 7,
-      title: "Budget Room in Chennai",
-      location: "T Nagar, Chennai",
-      price: "₹900/night",
-      rating: 4.2,
-      image: "https://plus.unsplash.com/premium_photo-1673014201674-a22222a0a454?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title: "Student PG near LPU Campus",
+      location: "Phagwara, Punjab",
+      price: "₹4,800/month",
+      rating: 3.9,
+      image: "https://media.istockphoto.com/id/2198113175/photo/interior-of-vintage-style-bedroom-with-with-window-curtains.jpg?s=612x612&w=0&k=20&c=94lLPf67kJsR2Csa0J5eiZyvsos4Urx4D_OibXq9lh8="
+    ,type: "Student PG",
     },
     {
       id: 8,
-      title: "Terrace Studio in Hyderabad",
-      location: "Banjara Hills, Hyderabad",
-      price: "₹1,700/night",
-      rating: 4.4,
-      image: "https://plus.unsplash.com/premium_photo-1661880248185-353cd7ffe1cf?q=80&w=1596&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      title: "Girls PG in Salt Lake Sector V",
+      location: "Kolkata, West Bengal",
+      price: "₹6,800/month",
+      rating: 4.3,
+      image: "https://media.istockphoto.com/id/1395224980/photo/a-holy-city-north-india-filled-with-temples-and-religious-places.jpg?s=612x612&w=0&k=20&c=0mLTGDaGiuifFJko9sC0Xo-bWSNoFXQxgrtK5N4NMco="
+      ,type: "Girls PG",
     }
   ];
-  
-  
+
+
 
   const categories = ["Popular", "Beach", "Mountain", "City", "Cabin", "Unique"]
 
